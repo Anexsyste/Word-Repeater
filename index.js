3437f70311793c7cf0cb27608d92fa9e75d1edc9
Anexsyste <122510054+Anexsyste@users.noreply.github.com>
2023-01-24 14:24:20 -0800

    Style changes and added copy button

--- index.js
+++ index.js
@@ -3,20 +3,56 @@
 
 
 
-function play() {
+/*function play(str, num) {
     let wordEL = document.getElementById("word-el").value
-    document.getElementById("repeat-el").innerText = wordEL + " %0D%0A" + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL 
+    if (num < 21){
+        return str.repeat(num);
 
+    }else {
+        return "";
+    }
 
-}
-
-function remove(){
-    wordEL = document.getElementById("word-el").value = ""
+    /*let wordEL = document.getElementById("word-el").value
     document.getElementById("repeat-el").innerText = wordEL
-}
+    return str;*/
+    
+    
+    /*let wordEL = document.getElementById("word-el").value
+    document.getElementById("repeat-el").innerText = wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL 
+*/
 
-for(var p = 0; p < 21; p++){
-    let wordEL = document.getElementById("word-el").value
+   /* var numberOfTimes = 21;
+    for(var p = 0; p < numberOfTimes; p++){
+        let wordEL = document.getElementById("word-el").value 
+         document.getElementById("repeat-el").write(wordEL + "<br>" + wordEL)
+   */
+   
+    /*    let wordEL = document.getElementById("word-el").value
     document.getElementById("repeat-el").innerText = wordEL
     console.log(wordEL)
 }
+
+}*/
+
+function play(){
+    let wordEL = document.getElementById("word-el").value
+    document.getElementById("repeat-el").innerText = wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + "\n" + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + "\n" + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + "\n" + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + "\n"   
+}
+
+
+function remove(){
+    wordEL = document.getElementById("word-el").value = ""
+    document.getElementById("repeat-el").innerText = wordEL + "Word will appear here"
+}
+
+function copyFunction(){
+    const copyText = document.querySelector(".myInput").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    k2button.innertext = "text copied";
+    textArea.remove();
+}
+document.getElementById("k2button").addEventListener('click', copyFunction)
