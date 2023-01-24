c61450769ee4faadff1ed9f6044a8996bd81417a
Anexsyste <122510054+Anexsyste@users.noreply.github.com>
2023-01-23 22:33:32 -0800

    Finally made it work

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-let repeatEL = document.getElementById("repeat-el")
-let wordEL = document.getElementById("word-el")
+//let repeatEL = document.getElementById("repeat-el")
+//let wordEL = document.getElementById("word-el").innerText
 
 
 
-function repeat() {
-    repeatEL.textContent = 1
-    console.log(repeatEL)
+function play() {
+    let wordEL = document.getElementById("word-el").value
+    document.getElementById("repeat-el").innerText = wordEL + " %0D%0A" + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL + " " + wordEL 
+
+
 }
 
+function remove(){
+    wordEL = document.getElementById("word-el").value = ""
+    document.getElementById("repeat-el").innerText = wordEL
+}
+
+for(var p = 0; p < 21; p++){
+    let wordEL = document.getElementById("word-el").value
+    document.getElementById("repeat-el").innerText = wordEL
+    console.log(wordEL)
+}
